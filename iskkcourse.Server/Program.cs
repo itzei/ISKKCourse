e8be1465c500fec815b491bb54c9a49aea303854
itzei <eimantas.pyragis333@gmail.com>
2025-01-13 16:49:51 +0200

    Update database conf

--- iskkcourse.Server/Program.cs
+++ iskkcourse.Server/Program.cs
@@ -11,7 +11,8 @@ var config = builder.Configuration;
 var mysqlDb = config["MySQL:Db"];
 var mysqlUser = config["MySQL:User"];
 var mysqlPassword = config["MySQL:Password"];
-var mysqlConn = $"server=localhost;port=3306;user={mysqlUser};password={mysqlPassword};database={mysqlDb};CharSet=utf8;TreatTinyAsBoolean = false";
+var mysqlServer = config["MySQL:Server"];
+var mysqlConn = $"server={mysqlServer};port=3306;user={mysqlUser};password={mysqlPassword};database={mysqlDb};CharSet=utf8;TreatTinyAsBoolean = false";
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(mysqlConn, ServerVersion.AutoDetect(mysqlConn)
     ));
